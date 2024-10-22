32188da2f4e7d1722858f1033b665398866de9b8
Erick Yuri Pereira <132232819+erickyuri159@users.noreply.github.com>
2024-10-22 15:30:28 -0300

    att
    coisa nova

--- Assets/Scripts/Dialogo.cs
+++ Assets/Scripts/Dialogo.cs
@@ -9,6 +9,7 @@ using UnityEngine.SceneManagement;
 public class Dialogo : MonoBehaviour
 {
     public string[] dialogueNpc;
+    public Sprite[] dialogueImages; // Imagens correspondentes ao diálogo
     public int dialogueIndex;
 
     public GameObject dialoguePanel;
@@ -20,14 +21,16 @@ public class Dialogo : MonoBehaviour
 
     public bool readyToSpeak;
     public bool startDialogue = true;
+    public float textSpeed = 0.04f; // Velocidade do texto
+    public Button startGameButton; // Referência ao botão de iniciar o jogo
 
 
 
     void Start()
     {
+        startGameButton.gameObject.SetActive(false);// Esconde o botão no início
+        StartDialogue();
 
-            StartDialogue();
- 
     }
     private void Update()
     {
@@ -47,10 +50,11 @@ public class Dialogo : MonoBehaviour
         }
         else
         {
-            dialoguePanel.SetActive(false);
+            // dialoguePanel.SetActive( false);
             startDialogue = false;
             dialogueIndex = 0;
-          
+            startGameButton.gameObject.SetActive(true);
+
         }
     }
     IEnumerator delayMensagem()
@@ -61,7 +65,7 @@ public class Dialogo : MonoBehaviour
 
     void StartDialogue()
     {
-        nameNpc.text = "Tono";
+        nameNpc.text = "Atayla";
         imageNpc.sprite = spriteNpc;
         startDialogue = true;
         dialogueIndex = 0;
@@ -71,16 +75,22 @@ public class Dialogo : MonoBehaviour
 
     IEnumerator showDialogue()
     {
-        dialogueText.text = "";
-        foreach (char letter in dialogueNpc[dialogueIndex])
         {
-            dialogueText.text += letter;
-            yield return new WaitForSeconds(0.05f);
+            dialogueText.text = "";
+            if (dialogueIndex < dialogueImages.Length)
+            {
+                imageNpc.sprite = dialogueImages[dialogueIndex]; // Atualiza a imagem conforme a frase
+            }
+            foreach (char letter in dialogueNpc[dialogueIndex])
+            {
+                dialogueText.text += letter;
+                yield return new WaitForSeconds(textSpeed); // Use textSpeed para controlar a velocidade
+
+            }
         }
+
     }
 
-    
 
-   
-    
+
 }
