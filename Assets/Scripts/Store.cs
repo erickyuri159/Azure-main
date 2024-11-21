54e7f863a5ff5d2b8c14c813648989aa346a6c57
Erick Yuri Pereira <132232819+erickyuri159@users.noreply.github.com>
2024-11-21 16:16:27 -0300

    1

--- Assets/Scripts/Store.cs
+++ Assets/Scripts/Store.cs
@@ -25,7 +25,7 @@ public class Store : MonoBehaviour
 
     public void BuyMagnetUpgrade()
     {
-        int magnetUpgradeCost = 50;
+        int magnetUpgradeCost = 1500;
 
         if (CJ.moedas >= magnetUpgradeCost)
         {
